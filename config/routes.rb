835b3688740aa25b2dd5245c8fd0ee7b2b5b6569
cure10 <soccercure112@gmail.com>
2021-04-06 05:06:15 +0000

    [Update]3-8submisson3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'homes#top'
-  resources :books
+  #resources :books
   get 'top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as:'book'
 end
