476ec55db0e646a46a7876ec00bfefc79a047858
cure10 <soccercure112@gmail.com>
2021-04-07 06:03:07 +0000

    [add]バリデーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id),notice: "successfully"
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to @book,notice: "successfully"
+    else
+    @books=Book.all
+    render :index
+    end
   end
 
   def show
